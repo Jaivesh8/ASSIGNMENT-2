4c78ce9669429e44d6c176962ee33a14739ad922
JAIVESH CHOPRA <jaivesh50@gmail.com>
2025-08-31 20:23:26 +0530

    Q6b.cpp

--- ASSIGNMENT 2/Q6b.cpp
+++ ASSIGNMENT 2/Q6b.cpp
@@ -1,97 +1,74 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-     int r,c,r2,c2;
-    cout<<"enter no of rows of 1";
-    cin>>r;
-    cout<<"enter no of cols of 1";
-    cin>>c;
-    vector<vector<int>> triplet1;
-    cout<<"enter no of rows of 2";
-    cin>>r2;
-    cout<<"enter no of cols of 2";
-    cin>>c2;
-    vector<vector<int>> triplet2;
-    int arr1[r][c];
-    int arr2[r2][c2];
-    vector<vector<int>> triplet3;
-    cout << "Enter elements: ";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++)
-        {
-           cin>> arr1[i][j]; 
-        }
-        cout<<endl;
-          
-    }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++)
-        {
-           if(arr1[i][j]!=0){
-            triplet1.push_back({i,j,arr1[i][j]});
-           }
-        }
-        cout<<endl;
-          
-    }
-    
-    cout<<"enter elements of arr 2";
-    for (int i = 0; i < r2; i++) {
-        for (int j = 0; j < c2; j++)
-        {
-           cin>> arr2[i][j]; 
-        }
-        cout<<endl;
-          
-    }
-    for (int i = 0; i < r2; i++) {
-        for (int j = 0; j < c2; j++)
-        {
-           if(arr2[i][j]!=0){
-            triplet2.push_back({i,j,arr2[i][j]});
-           }
-        }
-        cout<<endl;
-          
-    }
-   if (r != r2 || c != c2) {
-    cout << "Addition not possible!" << endl;
-    return 0;
-}
-int i = 0, j = 0;
-while (i < triplet1.size() && j < triplet2.size()) {
-    // Case 1: triplet1[i] comes before triplet2[j]
-    if (triplet1[i][0] < triplet2[j][0] ||
-       (triplet1[i][0] == triplet2[j][0] && triplet1[i][1] < triplet2[j][1])) {
-        triplet3.push_back(triplet1[i]);
-        i++;
-    }
-    // Case 2: triplet2[j] comes before triplet1[i]
-    else if (triplet2[j][0] < triplet1[i][0] ||
-            (triplet2[j][0] == triplet1[i][0] && triplet2[j][1] < triplet1[i][1])) {
-        triplet3.push_back(triplet2[j]);
-        j++;
-    }
-    // Case 3: Same row and col → add values
-    else {
-        int sum = triplet1[i][2] + triplet2[j][2];
-        if (sum != 0) {
-            triplet3.push_back({triplet1[i][0], triplet1[i][1], sum});
-        }
-        i++; j++;
-    }
-}
-  while (i < triplet1.size()) {
-    triplet3.push_back(triplet1[i++]);
-}
-while (j < triplet2.size()) {
-    triplet3.push_back(triplet2[j++]);
-}
-cout << "Resultant triplet";
-    for (auto &t : triplet3) {
-        cout << t[0] << " " << t[1] << " " << t[2] << "\n";
-    }
+    int m, n, t1, t2;
+    cout << "Enter rows and cols of matrices: ";
+    cin >> m >> n;
 
+    cout << "Enter number of non-zero elements in A: ";
+    cin >> t1;
+    int A[100][3];
+    A[0][0] = m; A[0][1] = n; A[0][2] = t1;
+    cout << "Enter triplet form of A (row col val):\n";
+    for (int i = 1; i <= t1; i++){
+         cin >> A[i][0] >> A[i][1] >> A[i][2];
+    }
+    cout << "Enter number of non-zero elements in B: ";
+    cin >> t2;
+    int B[100][3];
+    B[0][0] = m; B[0][1] = n; B[0][2] = t2;
+    cout << "Enter triplet form of B (row col val):\n";
+    for (int i = 1; i <= t2; i++){
+         cin >> B[i][0] >> B[i][1] >> B[i][2];
+    }
+    int C[200][3];  
+    C[0][0] = m; C[0][1] = n;
+    int i = 1, j = 1, k = 1;
+
+    while (i <= t1 && j <= t2) {
+        if (A[i][0] < B[j][0] || (A[i][0] == B[j][0] && A[i][1] < B[j][1])) {
+           
+            C[k][0] = A[i][0];
+            C[k][1] = A[i][1];
+            C[k][2] = A[i][2];
+            i++; k++;
+        }
+        else if (B[j][0] < A[i][0] || (B[j][0] == A[i][0] && B[j][1] < A[i][1])) {
+           
+            C[k][0] = B[j][0];
+            C[k][1] = B[j][1];
+            C[k][2] = B[j][2];
+            j++; k++;
+        }
+        else {
+            
+            int sum = A[i][2] + B[j][2];
+            if (sum != 0) {
+                C[k][0] = A[i][0];
+                C[k][1] = A[i][1];
+                C[k][2] = sum;
+                k++;
+            }
+            i++; j++;
+        }
+    }
+    while (i <= t1) {
+        C[k][0] = A[i][0];
+        C[k][1] = A[i][1];
+        C[k][2] = A[i][2];
+        i++; k++;
+    }
+    while (j <= t2) {
+        C[k][0] = B[j][0];
+        C[k][1] = B[j][1];
+        C[k][2] = B[j][2];
+        j++; k++;
+    }
+    C[0][2] = k-1;  
+    cout << "Resultant triplet matrix C:\n";
+    for (int p = 0; p <= C[0][2]; p++) {
+        cout << C[p][0] << " " << C[p][1] << " " << C[p][2] << "\n";
+    }
     return 0;
 }
