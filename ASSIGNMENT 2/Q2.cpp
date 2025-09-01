98d7db7ecee3b2b5e4adb63cc5a6191a68949024
JAIVESH CHOPRA <jaivesh50@gmail.com>
2025-09-01 11:17:27 +0530

    Q2.cpp

--- ASSIGNMENT 2/Q2.cpp
+++ ASSIGNMENT 2/Q2.cpp
@@ -1,28 +1,30 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-     int n;
-    cout<<"enter size of array";
-    cin>>n;
-    vector<int> arr(n);
-    cout<<"enter array values";
-    for (int i = 0; i <n; i++)
-    {
-        cin>>arr[i];
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n-1-i; j++)
-        {
-            if(arr[j+1]<arr[j])
-           swap(arr[j+1],arr[j]);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-        
     }
-    for (int i = 0; i <n; i++)
-    {
-        cout<<arr[i]<<endl;
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    cout << "\n";
     }
-    
+
     return 0;
 }
