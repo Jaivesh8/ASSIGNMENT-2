d32a645ff5e1cd8a5370d7e86e07782eb1a920fd
JAIVESH CHOPRA <jaivesh50@gmail.com>
2025-09-01 11:12:57 +0530

    Q1.cpp

--- ASSIGNMENT 2/Q1.cpp
+++ ASSIGNMENT 2/Q1.cpp
@@ -1,41 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cout<<"enter size of array";
-    cin>>n;
-    vector<int> arr(n);
-    cout<<"enter array values";
-    for (int i = 0; i <n; i++)
-    {
-        cin>>arr[i];
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n]; 
+    cout << "Enter array values (sorted) ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    
-    int mid;
     int target;
-    cout<<"enter target element";
-    cin>>target;
-    int low=0;
-    int high=n-1;
-    int found=0;
-    while(low<=high){
-        int mid=(low+high)/2;
-        if(arr[mid]==target){
-            cout<<"target found at"<<mid;
-            found=1;
+    cout << "Enter target element ";
+    cin >> target;
+    int low = 0, high = n - 1;
+    int found = 0;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target) {
+            cout << "Target found at index " << mid << "\n";
+            found = 1;
             break;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
-        else if (arr[mid]<target){
-            low=mid+1;
-        }
-        else if(arr[mid]>target){
-            high=mid-1;
-        }
-        
     }
-    if(found==0){
-        cout<<"target not found";
+
+    if (found == 0) {
+        cout << "Target not found\n";
     }
-    
+
     return 0;
 }
